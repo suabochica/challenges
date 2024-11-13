7c53ad5186c869c70572fa0729214312945d9b4c
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-11-13 11:02:19 -0500

    org: push relocated files.

--- packages/file-processing/src/support/logs.ts
+++
@@ -1,9 +0,0 @@
-import { Effect, Express } from "../support/express";
-
-export const loggerMiddleware = Express.gen(function* (_) {
-  const { request, next } = yield* _(Express.DefaultContext);
-  const { method, path } = request;
-
-  yield* _(Effect.log(`${method} ${path}`));
-  yield* _(Effect.sync(next));
-});
